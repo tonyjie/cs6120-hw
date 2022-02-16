9579c28c89ff834414b14c9cf0ddce708d38f027
tonyjie <jiajieli0429@gmail.com>
2022-02-16 16:20:58 -0500

    [Lesson 4] Live Variables and a general framework

--- lesson4/README.md
+++ lesson4/README.md
@@ -4,3 +4,10 @@
 cd test/defined
 turnt *.bril
 ```
+Currently, it doesn't support function with arguments directly for reaching definition. 
+
+## Live Variables
+```
+cd test/live
+turnt *.bril
+```
--- lesson4/df.py
+++ lesson4/df.py
@@ -1,5 +1,6 @@
 import sys
 import json
+from typing import Tuple, Callable
 from utils import form_blocks, fresh
 
 def get_block_name(blocks: list) -> dict:
@@ -56,9 +57,9 @@ def find_predecessor(cfg: dict, curr_label: str) -> list:
             predec_labels.append(label)
     return predec_labels
 
-def transfer(block: list, In: set) -> set:
+def defined_func(block: list, In: set) -> set:
     '''
-    Transfer function. 
+    Forward Transfer function: Reaching Definition. 
     Go through a block with several instrs. Get the Out of the block, given In. 
     '''
 
@@ -76,12 +77,36 @@ def transfer(block: list, In: set) -> set:
 
     return Out
 
+def live_func(block: list, Out: set) -> set:
+    '''
+    Backward Transfer function: Live Variables. 
+    A variable is live at some point if it holds a vlue that may be needed in the future, or equivalently if its value may be read before the next time the variable is written to. 
+    '''
+
+    # Transfer function: In(Out) = (Used(b)) Union (Out - Def(b)). 
+    # Do it each line from the last instr to the first instr, so that we can handle `a=a+1`. --> a is still a live variable. 
+    In = Out.copy()
+
+    for instr in reversed(block): # from the last instr -> first instr
+        if 'dest' in instr:
+            dest = instr['dest']
+            In.discard(dest) # - Def
+        if 'args' in instr:
+            args = instr['args']
+            In.update(args)
+
+    return In
+    
+
 def print_df(In: dict, Out: dict):
+    '''
+    Print the result of Dataflow Analysis based on certain format. 
+    '''
     for block_label in In.keys():
         in_var = In[block_label]
         out_var = Out[block_label]
         in_var = ', '.join(sorted(in_var)) if (len(in_var) != 0) else '∅'
-        out_var = ', '.join(sorted(out_var)) if (len(in_var) != 0) else '∅'
+        out_var = ', '.join(sorted(out_var)) if (len(out_var) != 0) else '∅'
 
         print(f"{block_label}:")
         # print(f"  in:  {', '.join(sorted(in_var))}")
@@ -90,39 +115,12 @@ def print_df(In: dict, Out: dict):
         print(f"  out: {out_var}")
 
 
-def run_df(func):
+def forward_worklist(worklist: set, cfg: dict, block_labels: dict, blocks: list, In: dict, Out: dict, transfer: Callable) -> Tuple[dict, dict]:
     '''
-    blocks: list. Index -> Block.
-
-    block_labels: dict. Key: Index; Value: Label name of the block. 
-
-    In: dict. Key: Label; Value: variables (set)
-    Out: dict. Key: Label; Value: variables (set)
-    worklist: Set. Elements are Labels of *blocks*. 
-    
-    cfg: dict. Key: label; Value: list of labels. 
+    Worklist algorithms with forward propagation. 
+    Args: worklist, cfg, block_labels, blocks, In, Out
+    Return: (In, Out)
     '''
-    blocks = list(form_blocks(func['instrs']))
-    
-    # get block name for each block
-    block_labels = get_block_name(blocks)
-
-    cfg = get_cfg(blocks, block_labels)
-    # print(cfg)
-
-    # worklist = all blocks
-    worklist = set(block_labels.values())
-    # print(worklist)
-
-    # initialization
-    In = dict()
-    Out = dict()
-
-    for labels in block_labels.values():
-        In[labels] = set()
-        Out[labels] = set()
-    
-
     while len(worklist) > 0: # not empty
         curr_label = worklist.pop() # pick a block from worklist
 
@@ -141,6 +139,7 @@ def run_df(func):
         if DEBUG:
             print(f"Before: In: {In[curr_label]}, Out: {Out[curr_label]}")
         Out[curr_label] = transfer(curr_block, In[curr_label]) # transfer function
+
         if Out[curr_label] != Out_old: # out[b] changed
             worklist.update(cfg[curr_label]) # add sucessors of b
         
@@ -148,15 +147,97 @@ def run_df(func):
             print(f"After: In: {In[curr_label]}, Out: {Out[curr_label]}")
             print(f"Worklist: {worklist}")
             print("\n")
+    
+    return In, Out
+
+
+def backward_worklist(worklist: set, cfg: dict, block_labels: dict, blocks: list, In: dict, Out: dict, transfer: Callable) -> Tuple[dict, dict]:
+    '''
+    Worklist algorithms with backward propagation. 
+    Args: worklist, cfg, block_labels, blocks, In, Out
+    Return: (In, Out)
+    '''
+    while len(worklist) > 0: # not empty
+        curr_label = worklist.pop() # pick a block from worklist
+
+        if DEBUG:
+            print(f"Curr label: {curr_label}")
+        for succ_label in cfg[curr_label]:
+            Out[curr_label] = Out[curr_label].union(In[succ_label]) # merge function: Union
+
+
+        block_index = list(block_labels.keys())[list(block_labels.values()).index(curr_label)] # find key by value in dict (block_labels)
+        curr_block = blocks[block_index]
+        
+        In_old = In[curr_label] # set. old In[b]
+
+        if DEBUG:
+            print(f"Before: In: {In[curr_label]}, Out: {Out[curr_label]}")
+        In[curr_label] = transfer(curr_block, Out[curr_label]) # transfer function
+
+        if In[curr_label] != In_old: # in[b] changed
+            worklist.update(find_predecessor(cfg, curr_label)) # add predecessors of b
+        
+        if DEBUG:
+            print(f"After: In: {In[curr_label]}, Out: {Out[curr_label]}")
+            print(f"Worklist: {worklist}")
+            print("\n")
+    
+    return In, Out    
+
+
+
+def run_df(func, analysis):
+    '''
+    Run dataflow analysis given the analysis method. 
+
+    Data sturctures;
+        blocks: list. Index -> Block.
+        block_labels: dict. Key: Index; Value: Label name of the block. 
+        In: dict. Key: Label; Value: variables (set)
+        Out: dict. Key: Label; Value: variables (set)
+        worklist: Set. Elements are Labels of *blocks*. 
+        cfg: dict. Key: label; Value: list of labels. 
+    '''
+    transfer_func = analysis[0]
+    worklist_algo = analysis[1]
+
+
+    blocks = list(form_blocks(func['instrs']))
+    
+    # get block name for each block
+    block_labels = get_block_name(blocks)
+
+    cfg = get_cfg(blocks, block_labels)
+
+    # worklist = all blocks
+    worklist = set(block_labels.values())
+
+    # initialization
+    In = dict()
+    Out = dict()
+
+    for labels in block_labels.values():
+        In[labels] = set()
+        Out[labels] = set()
+    
+    # worklist algorithm
+    In, Out = worklist_algo(worklist, cfg, block_labels, blocks, In, Out, transfer_func)
 
     print_df(In, Out)
             
 
-
-
 DEBUG = False
 
+ANALYSIS = {
+    'defined': (defined_func, forward_worklist),
+    'live': (live_func, backward_worklist)
+}
+
 if __name__ == "__main__":
+    if (len(sys.argv) > 1):
+        analysis = ANALYSIS[sys.argv[1]]
+
     prog = json.load(sys.stdin)
     for func in prog['functions']:
-        run_df(func)
+        run_df(func, analysis)
